c35050cc07aec4ac0b5130b7c0a1da71a17ee6b0
Touhou Engie <00_sling_shears@icloud.com>
2026-03-18 17:57:02 -0400

    add a domain flag to cookies to sync cursors between subdomains

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ time();
 setWindows();
 getLatestCommitId();
 setVolume();
-// setOutsideCheckbox();
+setOutsideCheckbox();
 
 
 // the lone dropdown menu (not very lonely anymore)
@@ -67,7 +67,7 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";" + "domain=touhouengie.com" + expires + ";path=/";
     configureSettings();
 }
 
