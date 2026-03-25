e418efe2118eb7956883d9d61481ce281fd13101
Touhou Engie <00_sling_shears@icloud.com>
2026-03-24 21:39:44 -0400

    ??

--- script.js
+++ script.js
@@ -281,6 +281,7 @@ function refreshToHomeScreen() {
 
 function pythonStart() {
   $("#pythonui").html(`<iframe class="w-full sm:w-150 h-120" src="https:/www.touhouengie.com/python"></iframe>`);
+  console.log("you there?")
 } 
 
 async function musicplayerStart() {
