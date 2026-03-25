8f6f77c268636091897913f60a9e1fdd5cfbb0ea
George W. <00_sling_shears@icloud.com>
2026-03-24 22:16:00 -0400

    adjust again

--- script.js
+++ script.js
@@ -280,7 +280,7 @@ function refreshToHomeScreen() {
 }
 
 function pythonStart() {
-  $("#pythonui").html(`<iframe class="w-full sm:w-150 h-full sm:h-120" src="https://www.touhouengie.com/python/index.html"></iframe>`);
+  $("#pythonui").html(`<iframe class="w-full sm:w-140 h-full sm:h-110" src="https://www.touhouengie.com/python/index.html"></iframe>`);
 } 
 
 async function musicplayerStart() {
